a7380e520a20d061dc5e9943a85a360f1dc22dc4
Enrique <35365013+Quaque17@users.noreply.github.com>
2026-08-23 12:03:22 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,8 @@
   <ul class="nav-links">
     <li><a href="nuestra-historia.html">Nuestra Historia</a></li>
     <li><a href="el-fin-de-semana.html">El Fin de Semana</a></li>
+    <li><a href="alojamientos.html">Alojamientos</a></li>
+    <li><a href="dudas.html">Dudas</a></li>
   </ul>
   <button class="hamburger" aria-label="Menú">
     <span></span><span></span><span></span>
