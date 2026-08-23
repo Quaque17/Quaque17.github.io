785bf90c529ba25535cbdcb5bbaf11f14f28296d
Enrique <35365013+Quaque17@users.noreply.github.com>
2026-08-23 20:29:24 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,7 @@
   const nav = `
 <nav>
   <a href="index.html" class="nav-logo">Quique &amp; Susana</a>
-  <ul class="nav-links">
-    <li><a href="nuestra-historia.html">Nuestra Historia</a></li>
+  <ul class="nav-links">    
     <li><a href="el-fin-de-semana.html">El Fin de Semana</a></li>
     <li><a href="alojamientos.html">Alojamientos</a></li>
     <li><a href="dudas.html">Dudas</a></li>
